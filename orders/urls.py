091083db519b7669735f0e7d1929914cc7a2d217
omariut <omarcpgcbl@gmail.com>
2021-11-14 17:46:42 +0600

    initial commit

--- orders/urls.py
+++ orders/urls.py
@@ -5,7 +5,7 @@ from orders.views import CreateOrderItem, EditCart, CheckOutView, ConfirmationVi
 app_name = 'orders'
 
 urlpatterns = [
-    path('home', CreateOrderItem.as_view(), name='add-to-cart'),
+    path('add-to-cart', CreateOrderItem.as_view(), name='add-to-cart'),
     path('', CreateOrderItem.as_view(), name='home'),
     path('cart-edit/<int:pk>', EditCart.as_view(), name='cart-edit'),
     path('checkout', CheckOutView.as_view(), name='checkout'),
