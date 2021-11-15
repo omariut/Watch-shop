198c07a6edc47d3d1004f3fc2d1b54346023a12a
omariut <omarcpgcbl@gmail.com>
2021-11-15 06:50:10 +0600

    initial commit

--- orders/get_context_mixin.py
+++ orders/get_context_mixin.py
@@ -1,11 +1,25 @@
 from customers.models import Customer
 from orders.models import Order, OrderItem, Address
 
+
 class GetContextMixin:
+    def get_current_order(self):
+        try:
+            order = Order.objects.get_or_create(
+                customer=self.request.user, complete=False
+            )
+        except:
+            try:
+                id = self.request.session["order_id"]
+                order = Order.objects.get(id=id)
+            except:
+                order = Order.objects.create(complete=False)
+                self.request.session["order_id"] = order.id
+        return order
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        order = Order.objects.get(customer=self.request.user, complete=False)
+        order = self.get_current_order()
         orderitems = order.orderitem_set.all()
         context["orderitems"] = orderitems
         context["order"] = order
--- orders/views.py
+++ orders/views.py
@@ -9,29 +9,29 @@ from django.urls import reverse_lazy
 from django.forms import inlineformset_factory
 from orders.get_context_mixin import GetContextMixin
 
-# Create your views here
 
-class CreateOrderItem(CreateView):
+class CreateOrderItem(GetContextMixin, CreateView):
     model = OrderItem
-    fields = ['product']
-    template_name = 'home.html'
-    success_url = reverse_lazy('orders:add-to-cart')
-    
+    fields = ["product"]
+    template_name = "home.html"
+    success_url = reverse_lazy("orders:add-to-cart")
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['products'] = Product.objects.all()
-        if self.request.user.is_authenticated: 
-            order, created = Order.objects.get_or_create(customer = self.request.user, complete=False )
-            orderitems = order.orderitem_set.all()
-            products_in_order = [orderitem.product for  orderitem in orderitems]
-            context['products_in_order'] =  products_in_order
-            context['order'] = order
+        context["products"] = Product.objects.all()
+        order = super().get_current_order()
+        orderitems = order.orderitem_set.all()
+        products_in_order = [orderitem.product for orderitem in orderitems]
+        context["products_in_order"] = products_in_order
+        context["order"] = order
         return context
-    
+
     def form_valid(self, form):
         product = form.instance.product
-        order,created = Order.objects.get_or_create(customer = self.request.user, complete=False )
-        orderitem,created = OrderItem.objects.get_or_create(order=order, product=product)
+        order = super().get_current_order()
+        orderitem, created = OrderItem.objects.get_or_create(
+            order=order, product=product
+        )
         orderitem.quantity = 1
         form.instance = orderitem
         return super().form_valid(form)
@@ -39,99 +39,43 @@ class CreateOrderItem(CreateView):
 
 class EditCart(UpdateView):
     model = Order
-    template_name = 'cart.html'
-    success_url = reverse_lazy('orders:add-to-cart')
-    
+    template_name = "cart.html"
+    success_url = reverse_lazy("orders:add-to-cart")
+
     def get_form_class(self):
         formset = inlineformset_factory(
-            Order, OrderItem, 
-            fields=('quantity',), 
+            Order,
+            OrderItem,
+            fields=("quantity",),
             extra=0,
-            )
+        )
         form_class = formset
         return form_class
-    
+
     def form_valid(self, form):
         form.save()
         print(self.success_url)
-        return redirect('orders:checkout')
+        return redirect("orders:checkout")
 
-class CheckOutView(GetContextMixin,CreateView ):
+
+class CheckOutView(GetContextMixin, CreateView):
     model = Address
-    fields = '__all__'
-    template_name = 'checkout.html'
-    success_url = reverse_lazy('orders:confirmation')
-
-    def form_valid(self, form):
-        
-        return super().form_valid(form)
-
-class ConfirmationView(GetContextMixin, TemplateView):
-    template_name='confirmation.html'
-
-    def post(self, request, *args, **kwargs):
-        order = Order.objects.get(customer=self.request.user, complete=False)
-        order.complete = True
-        order.save()
-        return super().post(request, *args, **kwargs)
+    fields = "__all__"
+    template_name = "checkout.html"
+    success_url = reverse_lazy("orders:confirmation")
 
 
+class ConfirmationView(TemplateView):
+    template_name = "confirmation.html"
 
-
-
-
-
-    
-
-
-
-    
-
-    
-
-
-
-
-    
-    
-
-
-
-
-def create_or_update_orderitem (request):
-    if request.method == 'POST':
-        product_id = request.POST.get('id')
-        quantity = request.POST.get('quantity')
-        print(quantity)
-    product = Product.objects.get(id=product_id)
-    customer,created = Customer.objects.get_or_create (user=request.user)
-    order,created = Order.objects.get_or_create(customer = customer )
-    orderitem,created = OrderItem.objects.get_or_create(order=order, product=product)
-    orderitem.quantity = quantity
-    orderitem.save()
-    data = order.get_total_item
-    return JsonResponse(data,safe=False)
-
-
-
-    def get_cart_total(request):
-
-        total_item = 0
-
-        if user.is_authenticated:
-            customer = Customer.objects.get (user=request.user)
-            order= Order.objects.get(customer = customer )
-            orderitem = OrderItem.objects.get(order=order)
-            for item in orderitem:
-                total_item += item.quantity
-            return total_item
-
-        else:
-            for name,quantity in request.session.items():
-                try:
-            
-                    product = Product.objects.get(name = name )
-                    total_price += product.unit_price * quantity
-                    return total_price 
-                except:
-                    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            order = Order.objects.get(customer=self.request.user, complete=False)
+        except:
+            id = self.request.session["order_id"]
+            order = Order.objects.get(id=id)
+        context["order"] = order
+        orderitems = order.orderitem_set.all()
+        context["orderitems"] = orderitems
+        return context
